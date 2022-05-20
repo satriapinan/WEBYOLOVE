db4da3d7ceaf51d5893ec27ba3c0877528b1578c
Satria Pinandita Abyatarsyah <99194983+satriapinan@users.noreply.github.com>
2022-05-21 02:55:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- Metadata -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Home</title>
+		<!-- Icon -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-primary">
+				<div class="container-sm container-md container-lg container-xl container-xxl">
+					<a class="navbar-brand" href="#">
+						<img src="img/logo.png" alt="logo yolove" width="100">
+					</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<li class="nav-item mx-2">
+								<a class="nav-link active fw-semibold" aria-current="page" href="#">Home</a>
+							</li>
+							<li class="nav-item me-2">
+								<a class="nav-link" href="#">Product</a>
+							</li>
+							<li class="nav-item me-3">
+								<a class="nav-link" href="#">About</a>
+							</li>
+						</ul>
+						<form class="w-100 me-4">
+							<div class="input-group">
+								<button type="submit" class="input-group-text text-black-50" id="search">
+									<i class="fa fa-search"></i>
+								</button>
+								<input name="search" class="form-control" type="search" placeholder="Search product.." aria-label="Search" aria-describedby="search">
+							</div>
+						</form>
+						<div class="d-flex">
+							<a href="" class="btn btn-outline-light me-2">Login</a>
+							<a href="" class="btn btn-light">Register</a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</header>
+		<footer class="container-fluid py-4 fixed-bottom bg-primary">
+			<div class="d-flex justify-content-center">
+				<a href="" class="me-3 text-secondary text-decoration-none fw-semibold">
+					<i class="fa fa-instagram me-1"></i>buketby_yolove
+				</a>
+				<a href="" class="text-secondary text-decoration-none fw-semibold">
+					<i class="fa fa-instagram me-1"></i>yolovecase
+				</a>
+			</div>
+		</footer>
+		
+		<!-- Script JS for Bootstrap -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+	</body>
+</html>
